b4bbe3093aea2c7d657da706f0ede1d1abe56305
xpl <rocket.mind@gmail.com>
2017-07-01 08:13:12 +0300

    minor code formatting changes

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ describe ('printable-characters', () => {
 		const { nonPrintableCharacters } = require ('./printable-characters')
 
 		const printableTextOnly = s => s.replace (nonPrintableCharacters, '')
-			, looksEmpty = s => printableTextOnly (s).length === 0
+		    , looksEmpty = s => printableTextOnly (s).length === 0
 
 		assert (!looksEmpty ('foobar'))
 		assert ( looksEmpty ('\u001b[106m  \t  \t   \n     \u001b[49m'))
@@ -24,5 +24,14 @@ describe ('printable-characters', () => {
 		    , foobar               = foobar_withANSICodes.replace (ansiEscapeCodes, '')
 
 		assert ('foobar' === foobar)
+	})
+
+	it ('matching printable characters works', () => {
+
+		const { ansiEscapeCodes, printableCharacters } = require ('./printable-characters')
+
+		const mask = s => s.replace (ansiEscapeCodes, '').replace (printableCharacters, '*')
+
+		assert.equal (mask ('{ foo:'), '* ****')
 	})
 })
