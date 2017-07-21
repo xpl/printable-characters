c08a5d95aad9a6c3932012c9fd6faf2ff20b8a35
xpl <rocket.mind@gmail.com>
2017-07-21 23:34:03 +0300

    redesigned from grounds up, TravisCI/Coveralls integration

--- test.js
+++ test.js
@@ -2,36 +2,69 @@ const assert = require ('assert')
 
 describe ('printable-characters', () => {
 
-    it ('counting visible letters works', () => {
+    it ('determines visible length', () => {
 
-		const { nonPrintableCharacters } = require ('./printable-characters')
+        const { strlen } = require ('./printable-characters')
 
-		const printableTextOnly = s => s.replace (nonPrintableCharacters, '')
-		    , looksEmpty = s => printableTextOnly (s).length === 0
+        assert (strlen ('foo bar'), 7)
+        assert (strlen ('\u001b[106mfoo bar\u001b[49m'), 7)
+    })
 
-		assert (!looksEmpty ('foobar'))
-		assert ( looksEmpty ('\u001b[106m  \t  \t   \n     \u001b[49m'))
-	})
+    it ('detects blank text', () => {
 
-	it ('removing ANSI codes works', () => {
+        const { isBlank } = require ('./printable-characters')
 
-		const { ansiEscapeCodes } = require ('./printable-characters')
+        assert (!isBlank ('foobar'))
+        assert ( isBlank ('\u001b[106m  \t  \t   \n     \u001b[49m'))
+    })
 
-		const brightCyanBg = '\u001b[106m'
-		    , noBgColor    = '\u001b[49m'
+    it ('matches zero-width characters and ANSI escape codes', () => {
 
-		const foobar_withANSICodes = brightCyanBg + 'foobar' + noBgColor
-		    , foobar               = foobar_withANSICodes.replace (ansiEscapeCodes, '')
+        const { ansiEscapeCodes, zeroWidthCharacters } = require ('./printable-characters')
 
-		assert ('foobar' === foobar)
-	})
+        let s = '\u001b[106m' + 'foo' + '\n\n' + 'bar' + '\u001b[49m'
 
-	it ('matching printable characters works', () => {
+        assert (s = s.replace (ansiEscapeCodes, ''), 'foo\n\nbar')
+        assert (    s.replace (zeroWidthCharacters, ''), 'foobar')
+    })
 
-		const { ansiEscapeCodes, printableCharacters } = require ('./printable-characters')
+    it ('obtains blank string of the same width', () => {
 
-		const mask = s => s.replace (ansiEscapeCodes, '').replace (printableCharacters, '*')
+        const { blank } = require ('./printable-characters')
 
-		assert.equal (mask ('{ foo:'), '* ****')
-	})
+        assert.equal (blank ('foo'), '   ')
+        assert.equal (blank ('\n'), '\n')
+        assert.equal (blank ('\t'), '\t')
+        assert.equal (blank ('\tfoo \nfoo'), '\t    \n   ')
+        assert.equal (blank ('\u001b[22m\u001b[1mfoo \t\u001b[39m\u001b[22m'), '    \t')
+    })
+
+    it ('extracts invisible parts followed by visible ones', () => {
+
+        const { partition } = require ('./printable-characters')
+
+        assert.deepEqual (partition (''),                        [                                                     ])
+        assert.deepEqual (partition ('foo'),                     [['',          'foo']                                 ])
+        assert.deepEqual (partition ('\u001b[1mfoo'),            [['\u001b[1m', 'foo']                                 ])
+        assert.deepEqual (partition ('\u001b[1mfoo\u0000bar'),   [['\u001b[1m', 'foo'],   ['\u0000', 'bar']            ])
+        assert.deepEqual (partition ('\u001b[1mfoo\u0000bar\n'), [['\u001b[1m', 'foo'],   ['\u0000', 'bar'], ['\n', '']])
+    })
+
+    it ('gets first N visible symbols (preserving invisible parts)', () => {
+
+        const { first } = require ('./printable-characters')
+
+        assert.equal (first ('123456789', 0),   '')
+        assert.equal (first ('123456789', 3),   '123')
+        assert.equal (first ('123456789', 100), '123456789')
+
+        const s = '\u001b[22m\u001b[1m' + '123' + '\u0000' + '45' + '\u001b[39m' + '67' + '\n' + '89' + '\u001b[39m\u001b[22m'
+        
+        assert.equal (first (s, 0),   '\u001b[22m\u001b[1m' + ''    + '\u0000' + ''   + '\u001b[39m' + ''   + '\n' + ''   + '\u001b[39m\u001b[22m')
+        assert.equal (first (s, 3),   '\u001b[22m\u001b[1m' + '123' + '\u0000' + ''   + '\u001b[39m' + ''   + '\n' + ''   + '\u001b[39m\u001b[22m')
+        assert.equal (first (s, 4),   '\u001b[22m\u001b[1m' + '123' + '\u0000' + '4'  + '\u001b[39m' + ''   + '\n' + ''   + '\u001b[39m\u001b[22m')
+        assert.equal (first (s, 6),   '\u001b[22m\u001b[1m' + '123' + '\u0000' + '45' + '\u001b[39m' + '6'  + '\n' + ''   + '\u001b[39m\u001b[22m')
+        assert.equal (first (s, 9),   '\u001b[22m\u001b[1m' + '123' + '\u0000' + '45' + '\u001b[39m' + '67' + '\n' + '89' + '\u001b[39m\u001b[22m')
+        assert.equal (first (s, 100), '\u001b[22m\u001b[1m' + '123' + '\u0000' + '45' + '\u001b[39m' + '67' + '\n' + '89' + '\u001b[39m\u001b[22m')
+    })
 })
